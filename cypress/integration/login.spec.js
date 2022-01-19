07941e2c9b7252158d292a2a15e5356f81a30821
Thyagokemer <thyagokemer@hotmail.com>
2022-01-19 20:13:44 -0300

    Novas alterações e arquivo novo - usando massa de dados

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,9 +1,10 @@
 /// <resference types="cypress" />
+const perfil = require('../fixtures/perfil.json')
 
 context('Funcionalidade login', () => {
 
     beforeEach(() => {
-        cy.visit (`http://lojaebac.ebaconline.art.br/minha-conta/`)
+        cy.visit ('minha-conta')
     });
 
     afterEach(() => {
@@ -11,16 +12,36 @@ context('Funcionalidade login', () => {
     });
 
     it('Deve fazer login com sucesso', () => {
-        cy.get('#username').type(`aluno_ebac@teste.com`)
-        cy.get('#password').type(`teste@teste.com`)
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click ()
 
         cy.get('.page-title').should('contain' , 'Minha conta')
     });  
 
+    it('Deve fazer login com sucesso - Usando arquivos de dados', () => {
+        cy.get('#username').type(perfil.Usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click ()
+
+        cy.get('.page-title').should('contain' , 'Minha conta')
+    });
+
+    it.only('Deve fazer login com sucesso - Usando fixture', () => {
+        cy.fixture('perfil').then( dados => { 
+        cy.get('#username').type(dados.Usuario)
+        cy.get('#password').type(dados.senha, {log: false})
+        cy.get('.woocommerce-form > .button').click ()
+
+        cy.get('.page-title').should('contain' , 'Minha conta')
+
+        } )
+        
+    });
+
     it('deve exbir uma mesagem de erro ao inserir ususario inválidos', () => {
-        cy.get('#username').type(`ebac@teste.com`)
-        cy.get('#password').type(`teste@teste.com`)
+        cy.get('#username').type('ebac@teste.com')
+        cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click ()
 
         cy.get('.woocommerce-error > li').should('contain', 'Endereço de e-mail desconhecido')
